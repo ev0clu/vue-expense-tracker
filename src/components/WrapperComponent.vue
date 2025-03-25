3eff4e1f259f87729cc67bdae31aaea99416ba41
ev0 <ev0clu@gmail.com>
2025-03-25 14:03:39 +0100

    WrapperComponent is created

---
+++ src/components/WrapperComponent.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="item">
+    <div class="header">
+      <slot name="header"></slot>
+    </div>
+    <div class="content">
+      <slot name="content"></slot>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.header {
+  font-size: x-large;
+  font-weight: 600;
+  border-bottom: 1px solid #e7e5e4;
+}
+
+.content {
+  width: 100%;
+}
+</style>
